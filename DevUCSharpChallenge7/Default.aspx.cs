db8248f4e50e5d6fe6b127c69b80bc4d0983f8a7
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-30 09:18:22 -0500

    first commit

---
+++ DevUCSharpChallenge7/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DevUCSharpChallenge7
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string result = "";
+
+            string[] names = new string[] {"Professor X", "Iceman", "Angel", "Beast", "Phoenix", "Cyclops", "Wolverine",
+            "Nightcrawler", "Storm", "Colossus"};
+
+            int[] numbers = new int[] { 7, 9, 12, 15, 17, 13, 2, 6, 8, 13 };
+
+            resultLabel.Text = result;
+        }
+    }
+}
